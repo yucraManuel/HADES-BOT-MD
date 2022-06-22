01ac3914d27db3fa649fd1cc0b3c1c6270b2faad
Yovanihades1212 <108025565+Yovanihades1212@users.noreply.github.com>
2022-06-22 15:36:51 -0500

    Add files via upload

---
+++ plugins/random-cat.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, text }) => {
+try {
+let res = await fetch('https://cataas.com/cat')
+let img = await res.buffer()
+let caption = `
+❍͜͡➣𝐇𝐀𝐃𝐄𝐒_𝐁𝐎𝐓❍͜͡➣
+`.trim()
+conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
+} catch (e) {
+console.log(e)
+throw '*Error!*'
+}}
+handler.help = ['cat']
+handler.tags = ['random']
+handler.command = /^cat$/i
+handler.fail = null
+export default handler
